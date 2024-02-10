f0d76f57357ae74db9c3991b40f2b9e57f73e5b4
wpseemol <wpseemol@gmail.com>
2024-02-11 00:45:10 +0600

    some test for change

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className="w-screen h-screen flex justify-center items-center text-5xl font-bold">
-            Coming Soon
+            Coming Soon...
         </div>
     );
 }
